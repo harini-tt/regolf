7d417ec1218f64cbbc135dbc48617b6c11675837
Michelle Wei <mjwei@mit.edu>
2025-08-10 12:21:24 -0700

    modal setup

--- hello_modal.py
+++ hello_modal.py
@@ -1,6 +1,9 @@
 import modal
 
-app = modal.App("hello-min")
+# Image that installs all dependencies listed in requirements.txt.
+image = modal.Image.debian_slim().pip_install_from_requirements("requirements.txt")
+
+app = modal.App("hello-min", image=image)
 
 @app.function()
 def hello(name: str = "world"):
